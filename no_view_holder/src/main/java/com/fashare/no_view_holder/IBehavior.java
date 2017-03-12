950dc916bc0e28faaf4fde05ee296ee23231b148
apple <153614131@qq.com>
2017-03-13 00:56:24 +0800

    refactor

--- no_view_holder/src/main/java/com/fashare/no_view_holder/IBehavior.java
+++ no_view_holder/src/main/java/com/fashare/no_view_holder/IBehavior.java
@@ -22,12 +22,12 @@ public interface IBehavior<A extends Annotation> {
 
     Class<A> belongsTo();
 
-    abstract class Simple<A extends Annotation, V extends View, DATA> implements IBehavior<A> {
+    abstract class Abstract<A extends Annotation, V extends View, DATA> implements IBehavior<A> {
         protected final String TAG = this.getClass().getCanonicalName();
         Class<A> mAnnotationClazz;
         Class<DATA> mValueClazz;
 
-        public Simple(Class<A> annotationClazz, Class<DATA> valueClazz) {
+        public Abstract(Class<A> annotationClazz, Class<DATA> valueClazz) {
             mAnnotationClazz = annotationClazz;
             mValueClazz = valueClazz;
         }
@@ -46,9 +46,8 @@ public interface IBehavior<A extends Annotation> {
 
                 if(mValueClazz.isAssignableFrom(value.getClass())) {
                     Log.d(TAG, value.toString());
-//                    onBind(itemView, annotation, mValueClazz.cast(value));
 
-                    bindIfNotNull((V) itemView.findViewById(getId(annotation)), annotation, (DATA)value, dataHolder);
+                    onBindAbstract(itemView, annotation, (DATA)value, dataHolder);
                 }else
                     throw new IllegalStateException(String.format("%s.%s which annotated by %s must be %s!!!",
                             dataHolder.getClass().getSimpleName(), field.getName(), mAnnotationClazz.getSimpleName(), mValueClazz.getSimpleName()));
@@ -57,20 +56,11 @@ public interface IBehavior<A extends Annotation> {
             }
         }
 
-        protected abstract @IdRes int getId(A annotation);
+        protected abstract void onBindAbstract(View itemView, A annotation, DATA value, Object dataHolder);
 
         public abstract void onBind(V targetView, A annotation, DATA value);
 
         protected void onBind(V targetView, A annotation, DATA value, Object dataHolder){}
-
-        public void bindIfNotNull(V targetView, A annotation, DATA value, Object dataHolder) {
-            if(targetView != null){
-                onBind(targetView, annotation, value);
-                onBind(targetView, annotation, value, dataHolder);
-            }else{
-                Log.e(TAG, String.format("%s with id(%d) is not found!!!", annotation.getClass().getSimpleName(), getId(annotation)));
-            }
-        }
 
         @Override
         public A getAnnotation(Field field) {
@@ -94,61 +84,36 @@ public interface IBehavior<A extends Annotation> {
         }
     }
 
-    abstract class Group<A extends Annotation, DATA> implements IBehavior<A>{
-        Class<A> mAnnotationClazz;
-        Class<DATA> mValueClazz;
-
-        public Group(Class<A> annotationClazz, Class<DATA> valueClazz) {
-            mAnnotationClazz = annotationClazz;
-            mValueClazz = valueClazz;
+    abstract class Simple<A extends Annotation, V extends View, DATA> extends Abstract<A, V, DATA> {
+        public Simple(Class<A> annotationClazz, Class<DATA> valueClazz) {
+            super(annotationClazz, valueClazz);
         }
 
         @Override
-        public boolean isApplyedOn(Field field) {
-            return getAnnotation(field) != null;
+        protected void onBindAbstract(View itemView, A annotation, DATA value, Object dataHolder) {
+            bindIfNotNull((V) itemView.findViewById(getId(annotation)), annotation, (DATA)value, dataHolder);
         }
 
-        @Override
-        public final void onBind(View itemView, Field field, Object dataHolder) {
-            A annotation = getAnnotation(field);
-            try {
-                field.setAccessible(true);
-                Object value = field.get(dataHolder);
-
-                if(mValueClazz.isAssignableFrom(value.getClass())) {
-                    Log.d(this.getClass().getSimpleName(), value.toString());
-//                    onBind(itemView, annotation, mValueClazz.cast(value));
-
-                    onBind(itemView, annotation, (DATA)value);
-                }else
-                    throw new IllegalStateException(String.format("%s.%s which annotated by %s must be %s!!!",
-                            dataHolder.getClass().getSimpleName(), field.getName(), mAnnotationClazz.getSimpleName(), mValueClazz.getSimpleName()));
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
+        public void bindIfNotNull(V targetView, A annotation, DATA value, Object dataHolder) {
+            if(targetView != null){
+                onBind(targetView, annotation, value);
+                onBind(targetView, annotation, value, dataHolder);
+            }else{
+                Log.e(TAG, String.format("%s with id(%d) is not found!!!", annotation.getClass().getSimpleName(), getId(annotation)));
             }
         }
 
-        public abstract void onBind(View itemView, A annotation, DATA value);
+        protected abstract @IdRes int getId(A annotation);
+    }
 
-        @Override
-        public A getAnnotation(Field field) {
-            return field.getAnnotation(mAnnotationClazz);
+    abstract class Group<A extends Annotation, DATA> extends Abstract<A, View, DATA>{
+        public Group(Class<A> annotationClazz, Class<DATA> valueClazz) {
+            super(annotationClazz, valueClazz);
         }
 
         @Override
-        public Class<A> belongsTo() {
-            return mAnnotationClazz;
-        }
-
-        // should override equals() and hashcode(), while using HashSet() on NoViewHolder.Options
-        @Override
-        public boolean equals(Object obj) {
-            return obj instanceof IBehavior && this.belongsTo() == ((IBehavior) obj).belongsTo();
-        }
-
-        @Override
-        public int hashCode() {
-            return this.belongsTo().hashCode();
+        protected void onBindAbstract(View itemView, A annotation, DATA value, Object dataHolder) {
+            onBind(itemView, annotation, value);
         }
     }
 }
